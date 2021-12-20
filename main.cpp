faf1a4f584bc34bef381582c4356e8343ac203c6
sifdev <86056885+sifdev@users.noreply.github.com>
2021-12-20 18:57:57 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int age;
+ string name;
+ cout<< "enter your name \n";
+ cin >> name;
+ cout <<"enter your age "  << endl;
+ cin>>  age;
+ cout<<"hi "<<name <<" you are "<<age<<endl;
+ 
+}
