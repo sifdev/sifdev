7c668b91a35fa1195cbcdce80a5fb8cac7f420f1
seif <86056885+sifdev@users.noreply.github.com>
2022-07-12 23:14:43 +0100

    Update main.cs

--- main.cs
+++ main.cs
@@ -18,14 +18,26 @@ class Car : Vehicle  // derived class (child)
 
 
 class Program {
+  
+  
+    public static int Fibonacci(int n) {
+    int a = 0;
+    int b = 1;
+    for (int i = 0; i < n; i++) {
+      int temp = a;
+      a = b;
+      b = temp + b;
+    }
+    return a;
+  }
   public static void Main (string[] args) {
 
-    long a= 92337203685587;
+    long m= 92337203685587;
 
     string s = "fd hd fh huu u dfu ejh dsu edd jv u nferjk iiifghebh hdfhuef u bjjjeufl hfifeuioe hey dfjjj jedfg nqieéab oia ";
 float f = 156454;
     Console.WriteLine(s);
-    Console.WriteLine(a);
+    Console.WriteLine(m);
     Console.WriteLine(f);
       Car myCar = new Car();
 
@@ -35,6 +47,9 @@ float f = 156454;
 
     // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
     Console.WriteLine(myCar.brand + " " + myCar.modelName);
+    for (int i = 0; i < 15; i++) {
+      Console.WriteLine(Fibonacci(i));
+    }
   }
     }
 }
