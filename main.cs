50f64fd36db893f60d62dc7f180beafa7ef4a6bf
seif <86056885+sifdev@users.noreply.github.com>
2022-07-12 23:10:14 +0100

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    long a= 92337203685587;
+
+    string s = "fd hd fh huu u dfu ejh dsu edd jv u nferjk iiifghebh hdfhuef u bjjjeufl hfifeuioe hey dfjjj jedfg nqieéab oia ";
+float f = 156454;
+    Console.WriteLine(s);
+    Console.WriteLine(a);
+    Console.WriteLine(f);
+    }
+}
