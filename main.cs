493b5e4b31df964ac4f0f46ac56c553f684953e4
seif <86056885+sifdev@users.noreply.github.com>
2022-07-12 23:11:37 +0100

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,5 +1,22 @@
 using System;
 
+
+
+class Vehicle  // base class (parent) 
+{
+  public string brand = "Ford";  // Vehicle field
+  public void honk()             // Vehicle method 
+  {                    
+    Console.WriteLine("Tuut, tuut!");
+  }
+}
+
+class Car : Vehicle  // derived class (child)
+{
+  public string modelName = "Mustang";  // Car field
+}
+
+
 class Program {
   public static void Main (string[] args) {
 
@@ -10,5 +27,13 @@ float f = 156454;
     Console.WriteLine(s);
     Console.WriteLine(a);
     Console.WriteLine(f);
+      Car myCar = new Car();
+
+    // Call the honk() method (From the Vehicle class) on the myCar object
+    myCar.honk();
+
+    // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
+    Console.WriteLine(myCar.brand + " " + myCar.modelName);
+  }
     }
 }
